f61eaa087f17fb79d86b634fc6179b22945a9fc5
Rafapg <rafa.pego84@gmail.com>
2018-02-23 21:14:34 -0600

    Agregando modulo de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,31 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
+import { LoginComponent } from './public/login/login.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    LoginComponent
   ],
   imports: [
+    //Core
     BrowserModule,
     RouterModule,
+    FormsModule, ReactiveFormsModule,
+    //Material
     BrowserAnimationsModule,
-    MatButtonModule
+    MatButtonModule,
+    MatFormFieldModule
+    //Custom
   ],
   providers: [],
   bootstrap: [AppComponent]
