3204b216ab5e68bf2d853c2739610bdbf3e85d59
Alex Arriaga <alex.arriaga.m@gmail.com>
2018-02-24 10:08:52 -0500

    Agregar MatInputModule a los imports del módulo principal de nuestra app de Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
+import { MatInputModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './common/header/header.component';
@@ -40,6 +41,7 @@ import { Ng2Webstorage } from 'ngx-webstorage';
     // Material
     MatButtonModule,
     MatFormFieldModule,
+    MatInputModule,
     // Custom
     Ng2Webstorage,
   ],
