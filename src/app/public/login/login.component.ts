d6bdd5af51939902e86f7caf9ddb65990b6fb39a
Rafael Pérez González <rafa.pego84@gmail.com>
2018-02-24 23:55:19 -0600

    Agregando logica para login

--- src/app/public/login/login.component.ts
+++ src/app/public/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, Validators } from '@angular/forms';
+import { AuthenticationService } from '../../common/services/authentication.service';
+import { LocalStorageService, SessionStorageService } from 'ngx-webstorage';
+import { Router } from '@angular/router';
+import { HttpResponse, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +15,9 @@ export class LoginComponent implements OnInit {
   username = new FormControl('', [Validators.required]);
   password = new FormControl('', [Validators.required]);
 
-  constructor() { }
+  constructor(public authService: AuthenticationService,
+              public locker: SessionStorageService,
+              public router: Router) { }
 
   getErrorMessageForUsername() {
     const hasError = this.username.hasError('required');
@@ -27,7 +33,23 @@ export class LoginComponent implements OnInit {
   }
 
   onSubmit(event: Event) {
+    console.log('estoy aqui');
     event.preventDefault();
+
+    this.authService.logIn(this.username.value, this.password.value)
+      .subscribe((data) => {
+        this.authService.user = data;
+        this.locker.store('user', data);
+        this.router.navigate(['/home']);
+
+      }, (error: HttpErrorResponse) => {
+        if (error.status === 406) {
+          console.error('Unable to login');
+        }
+
+        console.error(error);
+        this.authService.hasSession = false;
+      });
   }
 
 }
