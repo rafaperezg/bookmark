3ea59a388ac6db30e3d3e9bd95272664d9ae7e99
Rafael Pérez González <rafa.pego84@gmail.com>
2018-02-26 00:21:12 -0600

    Agregando interceptor

---
+++ src/app/auth/interceptors/token.interceptor.ts
@@ -0,0 +1,27 @@
+import { Injectable, Injector } from '@angular/core';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
+import { AuthenticationService } from '../../common/services/authentication.service';
+
+@Injectable()
+export class TokenInterceptor implements HttpInterceptor {
+
+  constructor(public injector: Injector) { }
+
+  intercept(request: HttpRequest<any>, next: HttpHandler ): Observable<HttpEvent<any>> {
+
+    const authService = this.injector.get(AuthenticationService);
+    const token = authService.user ? authService.user.api_token : '';
+
+    request = request.clone({
+      setHeaders : {
+        'Api-Token': token,
+        'Authorization': `User ${token}`,
+        'X-Request-With': 'XMLHttpRequest'
+      }
+    });
+
+    return next.handle(request);
+  }
+
+}
